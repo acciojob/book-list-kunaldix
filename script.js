c23a3288b73fd4f4e5b8132767753411d28f6f01
Kunal Dixit <kunaldixit15049@gmail.com>
2025-04-04 10:53:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+function addBook() {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let isbn = document.getElementById("isbn").value;
+
+  if (!title || !author || !isbn) {
+    alert("Please fill in all fields");
+    return;
+  }
+  let tableRow = document.getElementById("table-body");
+  tableRow.innerHTML += `
+  <tr class = "table-rows">
+          <td>${title}</td>
+          <td>${author}</td>
+          <td>${isbn}</td>
+          <td><span  class="material-symbols-outlined  delete-btn"> close </span></td>
+    </tr>
+        `;
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+}
+
+document.getElementById("table-body").addEventListener("click", function (e) {
+  if (e.target.classList.contains("delete-btn")) {
+    e.target.closest("tr").remove();
+  }
+});
